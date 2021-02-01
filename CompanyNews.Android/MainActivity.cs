e3cf3f138aea630e3a5d108b7709b6f4cdee3b4a
Uzeyir AYDIN <uzeyir.aydin@hotmail.com.tr>
2021-02-01 12:02:06 +0300

    Proje dosyası ekle.

---
+++ CompanyNews.Android/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Distribute;
+using Microsoft.AppCenter.Push;
+using Microsoft.AppCenter.Auth;
+using Microsoft.AppCenter.Data;
+namespace CompanyNews.Droid
+{
+    [Activity(Label = "CompanyNews", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            // APPCENTER
+            //AppCenter.Start("<app secret goes here>",
+            //       typeof(Analytics), typeof(Crashes), typeof(Push), typeof(Distribute));
+
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(enableFastRenderer: true);
+            FFImageLoading.Forms.Platform.CachedImageRenderer.InitImageViewHandler();
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ CompanyNews.UITests/Tests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace CompanyNews.UITests
+{
+    [TestFixture(Platform.Android)]
+    //[TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void FirstScreenLoadsProperly()
+        {
+            AppResult[] results = app.WaitForElement(c => c.Marked("Cafeteria is now open!"));
+            app.Screenshot("News Items screen.");
+
+            Assert.IsTrue(results.Any());
+        }
+
+        [Test]
+        public void TappingItemGoesToDetailsPage()
+        {
+            AppResult[] results = app.WaitForElement(c => c.Marked("Cafeteria is now open!"));
+            app.Screenshot("News Items screen.");
+
+            Assert.AreEqual(1, results.Length, "Expect only one element");
+
+            // Tap on the news item
+            app.Tap(c => c.Marked("Cafeteria is now open!"));
+
+            Thread.Sleep(1000);
+
+            app.Screenshot("Cafeteria details screen.");
+        }
+    }
+}
